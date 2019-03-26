5b7d57aadc0993d69a3297418260649294acb06d
Keel_im <kimh00335@gmail.com>
2019-03-26 22:49:35 +0900

    2019 03 26

---
+++ arducon/app/src/main/java/com/keelim/arducon/MainActivity.java
@@ -0,0 +1,63 @@
+package com.keelim.arducon;
+
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.Set;
+
+public class MainActivity extends AppCompatActivity {
+    static final int REQUEST_ENABLE_BT = 10;
+    int mPariedDeviceCount = 0;
+    Set<BluetoothAdapter> mDevices;
+
+    BluetoothAdapter mBluetoothAdapter;
+    BluetoothDevice mRemoteDevice;
+
+    BluetoothSocket mSocket = null;
+    OutputStream outputStream = null;
+    InputStream inputStream = null;
+
+
+
+    private TextView mTextMessage;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+                    mTextMessage.setText(R.string.title_home);
+                    return true;
+                case R.id.navigation_dashboard:
+                    mTextMessage.setText(R.string.title_dashboard);
+                    return true;
+                case R.id.navigation_notifications:
+                    mTextMessage.setText(R.string.title_notifications);
+                    return true;
+            }
+            return false;
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mTextMessage = (TextView) findViewById(R.id.message);
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+
+}
