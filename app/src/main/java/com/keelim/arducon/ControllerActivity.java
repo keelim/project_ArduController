1da3efd832d959f6650f2f41c3b3ea80c14307da
Keel_im <kimh00335@gmail.com>
2019-04-07 01:58:11 +0900

    2019 04 07

--- app/src/main/java/com/keelim/arducon/ControllerActivity.java
+++ app/src/main/java/com/keelim/arducon/ControllerActivity.java
@@ -1,9 +1,11 @@
 package com.keelim.arducon;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
+import android.view.MenuItem;
 
 public class ControllerActivity extends AppCompatActivity {
 
@@ -17,6 +19,31 @@ public class ControllerActivity extends AppCompatActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) { // 메뉴를 선택할 수 있게 한다.
         getMenuInflater().inflate(R.menu.actionbar_actions, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        Intent intent;
+        int id = item.getItemId();
+        switch (id) {
+            case R.id.action_bluetooth:
+                intent = new Intent(getApplicationContext(), MainActivity.class); //todo bluetooth
+                startActivity(intent);
+                break;
+            case R.id.action_main:
+                intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.action_controller:
+                intent = new Intent(getApplicationContext(), ControllerActivity.class);
+                startActivity(intent);
+                break;
+            default:
+                break;
+
+        }
+
         return true;
     }
 }
--- app/src/main/java/com/keelim/arducon/MainActivity.java
+++ app/src/main/java/com/keelim/arducon/MainActivity.java
@@ -4,12 +4,14 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     Button button_change;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,11 +28,38 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+
+
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.actionbar_actions, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        Intent intent;
+        int id = item.getItemId();
+        switch (id) {
+            case R.id.action_bluetooth:
+                intent = new Intent(getApplicationContext(), MainActivity.class); //todo bluetooth
+                startActivity(intent);
+                break;
+            case R.id.action_main:
+                intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.action_controller:
+                intent = new Intent(getApplicationContext(), ControllerActivity.class);
+                startActivity(intent);
+                break;
+            default:
+                break;
+
+        }
+
         return true;
     }
 }
