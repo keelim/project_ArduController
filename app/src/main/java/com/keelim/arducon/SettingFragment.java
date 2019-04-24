dfadc144e6920009fff32e9e2825e9c9ad11c1d2
keelim <kimh00335@gmail.com>
2019-04-24 21:42:46 +0900

    preference update

--- app/src/main/java/com/keelim/arducon/SettingFragment.java
+++ app/src/main/java/com/keelim/arducon/SettingFragment.java
@@ -1,12 +1,29 @@
 package com.keelim.arducon;
 
 import android.os.Bundle;
+import android.support.v7.preference.ListPreference;
 import android.support.v7.preference.PreferenceFragmentCompat;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 public class SettingFragment extends PreferenceFragmentCompat {
 
+    private ListPreference mListPreference;
+
+
     @Override
-    public void onCreatePreferences(Bundle bundle, String s) {
-        addPreferencesFromResource(R.xml.settings_preference);
+    public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+        // Indicate here the XML resource you created above that holds the preferences
+        setPreferencesFromResource(R.xml.settings_preference, rootKey);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        mListPreference = (ListPreference) getPreferenceManager().findPreference("preference_key");
+
+
+        return inflater.inflate(R.layout.main_fragment, container, false);
     }
 }
