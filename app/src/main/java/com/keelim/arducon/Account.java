8165223bd796611d6c8ef6a828464e7696bed41e
keelim <kimh00335@gmail.com>
2019-04-30 22:31:14 +0900

    2019 04 30 fragment setting fragment back button

--- app/src/main/java/com/keelim/arducon/Account.java
+++ app/src/main/java/com/keelim/arducon/Account.java
@@ -1,14 +1,18 @@
 package com.keelim.arducon;
 
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.util.Objects;
 
-public class Account extends Fragment {
+
+public class Account extends Fragment implements MainActivity.onKeyBackPressedListener {
 
 
     public Account() {
@@ -23,4 +27,20 @@ public class Account extends Fragment {
         return inflater.inflate(R.layout.fragment_account, container, false);
     }
 
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+
+        //메인뷰 액티비티의 뒤로가기 callback 붙이기
+        ((MainActivity) context).setOnKeyBackPressedListener(this);
+    }
+
+    @Override
+    public void onBackKey() {
+        MainActivity activity = (MainActivity) getActivity();
+        //액티비티의 콜백을 직접호출
+        Objects.requireNonNull(activity).onBackPressed();
+        activity.setOnKeyBackPressedListener(null);
+    }
 }
--- app/src/main/java/com/keelim/arducon/BugReport.java
+++ app/src/main/java/com/keelim/arducon/BugReport.java
@@ -1,14 +1,18 @@
 package com.keelim.arducon;
 
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.util.Objects;
 
-public class BugReport extends Fragment {
+
+public class BugReport extends Fragment implements MainActivity.onKeyBackPressedListener {
 
 
     public BugReport() {
@@ -22,5 +26,22 @@ public class BugReport extends Fragment {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_bug_report, container, false);
     }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+
+        //메인뷰 액티비티의 뒤로가기 callback 붙이기
+        ((MainActivity) context).setOnKeyBackPressedListener(this);
+    }
+
+    @Override
+    public void onBackKey() {
+        MainActivity activity = (MainActivity) getActivity();
+        //액티비티의 콜백을 직접호출
+        Objects.requireNonNull(activity).onBackPressed();
+        activity.setOnKeyBackPressedListener(null);
+    }
+
 
 }
