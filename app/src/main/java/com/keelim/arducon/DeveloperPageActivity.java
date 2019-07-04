9690edc7ed8ef6720f7ea6a16aa71b0316ecb3b5
keelim <kimh00335@gmail.com>
2019-07-04 19:49:44 +0900

    developer page change

--- app/src/main/java/com/keelim/arducon/DeveloperPageActivity.java
+++ app/src/main/java/com/keelim/arducon/DeveloperPageActivity.java
@@ -4,32 +4,12 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.vansuita.materialabout.builder.AboutBuilder;
-import com.vansuita.materialabout.views.AboutView;
-
 public class DeveloperPageActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        AboutView view = AboutBuilder.with(this)
-                .setPhoto(R.mipmap.profile_picture)
-                .setCover(R.mipmap.profile_cover)
-                .setName("Your Full Name")
-                .setSubTitle("Mobile Developer")
-                .setBrief("Android mobile developer")
-                .setAppIcon(R.mipmap.ic_launcher)
-                .setAppName(R.string.app_name)
-                .addGooglePlayStoreLink("")
-                .addGitHubLink("user")
-                .addFiveStarsAction()
-                .setVersionNameAsAppSubTitle()
-                .addShareAction(R.string.app_name)
-                .setWrapScrollView(true)
-                .setLinksAnimated(true)
-                .setShowAsCard(true)
-                .build();
-
         setContentView(R.layout.activity_developer_page);
+        Helper.with(this).init().loadAbout();
     }
 }
