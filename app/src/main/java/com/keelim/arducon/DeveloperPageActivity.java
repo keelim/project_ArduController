b0fe6d6625806628f7f8c87fc509d5d597f08b6d
keelim <kimh00335@gmail.com>
2019-07-04 12:25:50 +0900

    developer page change

---
+++ app/src/main/java/com/keelim/arducon/DeveloperPageActivity.java
@@ -0,0 +1,14 @@
+package com.keelim.arducon;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class DeveloperPageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_developer_page);
+    }
+}
