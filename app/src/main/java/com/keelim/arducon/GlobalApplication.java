0d445c297f2c0ef437a9434f16d04cdc1cde98cd
keelim <kimh00335@gmail.com>
2019-07-07 14:26:21 +0900

    kakao activity

--- app/src/main/java/com/keelim/arducon/GlobalApplication.java
+++ app/src/main/java/com/keelim/arducon/GlobalApplication.java
@@ -8,13 +8,21 @@ import com.kakao.auth.AuthType;
 import com.kakao.auth.IApplicationConfig;
 import com.kakao.auth.ISessionConfig;
 import com.kakao.auth.KakaoAdapter;
-import com.kakao.auth.KakaoSDK;
 
 public class GlobalApplication extends Application {
-    private static class KakaoSDKAdapter extends KakaoAdapter {
+    private static GlobalApplication instance;
+
+    public static final GlobalApplication getGlobalApplicationContext() {
+        if (instance == null)
+            throw new IllegalStateException("this application does not inherit com.kakao.GlobalApplication");
+        return instance;
+    }
+
+    protected static class KakaoSDKAdapter extends KakaoAdapter {
         /**
          * Session Config에 대해서는 default값들이 존재한다.
          * 필요한 상황에서만 override해서 사용하면 됨.
+         *
          * @return Session의 설정값.
          */
         @Override
@@ -22,7 +30,7 @@ public class GlobalApplication extends Application {
             return new ISessionConfig() {
                 @Override
                 public AuthType[] getAuthTypes() {
-                    return new AuthType[] {AuthType.KAKAO_LOGIN_ALL};
+                    return new AuthType[]{AuthType.KAKAO_LOGIN_ALL};
                 }
 
                 @Override
@@ -58,13 +66,9 @@ public class GlobalApplication extends Application {
         }
     }
 
-    private static Context getGlobalApplicationContext() {
-        return null;
-    }
-
     @Override
     public void onCreate() {
         super.onCreate();
-        KakaoSDK.init(new KakaoSDKAdapter());
+        instance = this;
     }
 }
