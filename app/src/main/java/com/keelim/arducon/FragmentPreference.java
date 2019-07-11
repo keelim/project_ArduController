d82d8b7a1f943852722527e60de15670552085b5
keelim <kimh00335@gmail.com>
2019-07-11 18:30:39 +0900

    preference fragment setting

---
+++ app/src/main/java/com/keelim/arducon/FragmentPreference.java
@@ -0,0 +1,14 @@
+package com.keelim.arducon;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class FragmentPreference extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fragment_preference);
+    }
+}
