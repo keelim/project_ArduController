9c6bf4089427f04fca86897575474c17477ddfa3
keelim <kimh00335@gmail.com>
2019-07-21 18:58:06 +0900

    setting activity

--- app/src/main/java/com/keelim/arducon/SettingActivity.java
+++ app/src/main/java/com/keelim/arducon/SettingActivity.java
@@ -26,7 +26,7 @@ public class SettingActivity extends AppCompatActivity { //fragment를 위한 �
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_setting);
+        setContentView(R.layout.activity_fgbasic);
         Toolbar toolbar = findViewById(R.id.toolbar_fragment);
 
         setSupportActionBar(toolbar);
