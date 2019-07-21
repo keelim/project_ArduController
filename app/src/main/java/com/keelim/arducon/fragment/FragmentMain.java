fac018cf8c21dc27d99bacd39bac1d059be8cef1
keelim <kimh00335@gmail.com>
2019-07-21 15:22:07 +0900

    fragment setting

--- app/src/main/java/com/keelim/arducon/fragment/FragmentMain.java
+++ app/src/main/java/com/keelim/arducon/fragment/FragmentMain.java
@@ -26,6 +26,8 @@ public class FragmentMain extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_main, container, false);
+
+
     }
 
 }
