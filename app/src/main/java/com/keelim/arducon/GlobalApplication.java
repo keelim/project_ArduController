27232d5d749464983f126eb6789f564e17a68dd0
keelim <kimh00335@gmail.com>
2019-07-30 01:40:50 +0900

    multi dex install

--- app/src/main/java/com/keelim/arducon/GlobalApplication.java
+++ app/src/main/java/com/keelim/arducon/GlobalApplication.java
@@ -1,17 +1,27 @@
-package com.keelim.arducon.utils;
+package com.keelim.arducon;
 
 import android.app.Application;
+import android.content.Context;
+
+import androidx.multidex.MultiDex;
 
 import com.kakao.auth.KakaoSDK;
 import com.keelim.arducon.adapters.KakaoSDKAdapter;
 
 public class GlobalApplication extends Application {
     private static GlobalApplication instance;
+
     public static GlobalApplication getGlobalApplicationContext() {
         if (instance == null) {
             throw new IllegalStateException("This Application does not inherit com.kakao.GlobalApplication");
         }
         return instance;
+    }
+
+    @Override
+    protected void attachBaseContext(Context base) {
+        super.attachBaseContext(base);
+        MultiDex.install(this);
     }
 
     @Override
