5a37b3b08d9f467685f8319e047278f0e57b2602
keelim <kimh00335@gmail.com>
2019-07-30 15:04:46 +0900

    2019 07 30

--- app/src/main/java/com/keelim/arducon/activities/Temp2Activity.java
+++ app/src/main/java/com/keelim/arducon/activities/Temp2Activity.java
@@ -1,16 +1,25 @@
 package com.keelim.arducon.activities;
 
+import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.keelim.arducon.R;
+import com.keelim.arducon.fragments.temp2.Temp2_1Fragment;
+import com.keelim.arducon.fragments.temp2.Temp2_2Fragment;
+import com.keelim.arducon.fragments.temp2.Temp2_3Fragment;
 
-public class Temp2Activity extends AppCompatActivity {
+public class Temp2Activity extends AppCompatActivity implements Temp2_1Fragment.OnFragmentInteractionListener, Temp2_3Fragment.OnFragmentInteractionListener, Temp2_2Fragment.OnFragmentInteractionListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_temp2);
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+        
     }
 }
