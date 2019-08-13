74fec3fbce143cef1767b19c977a7ccff4b3765a
keelim <kimh00335@gmail.com>
2019-08-13 17:10:17 +0900

    version change 2.0.13

--- app/src/main/java/com/keelim/arducon/activities/SplashActivity.java
+++ app/src/main/java/com/keelim/arducon/activities/SplashActivity.java
@@ -15,6 +15,7 @@ import io.fabric.sdk.android.Fabric;
 public class SplashActivity extends AppCompatActivity { //인트로 액티비티를 생성한다.
     private Handler handler;
 
+
     //인앱 업데이트 어디서 등록을 해야 하는가?
     Runnable runnable = new Runnable() {
         @Override
@@ -31,6 +32,8 @@ public class SplashActivity extends AppCompatActivity { //인트로 액티비티
         super.onCreate(savedInstanceState);
         Fabric.with(this, new Crashlytics());
         setContentView(R.layout.activity_intro);
+
+
 
         handler = new Handler();
         handler.postDelayed(runnable, 1000); //handler를 통하여 사용
