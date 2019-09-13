2f248fe9f17bfe4e53f9738ea6569dfee720f2bd
keelim <kimh00335@gmail.com>
2019-09-13 17:24:01 +0900

    code binding change

--- app/src/main/java/com/keelim/arducon/activities/DeveloperPageActivity.java
+++ app/src/main/java/com/keelim/arducon/activities/DeveloperPageActivity.java
@@ -3,16 +3,20 @@ package com.keelim.arducon.activities;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
 import com.keelim.arducon.R;
+import com.keelim.arducon.databinding.ActivityDeveloperPageBinding;
 import com.keelim.arducon.utils.MaterialHelper;
 
 public class DeveloperPageActivity extends AppCompatActivity {
+    ActivityDeveloperPageBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_developer_page);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_developer_page);
+        binding.setActivity(this);
         MaterialHelper.with(this).init().loadAbout();
     }
 }
--- app/src/main/java/com/keelim/arducon/activities/SettingActivity.java
+++ app/src/main/java/com/keelim/arducon/activities/SettingActivity.java
@@ -3,20 +3,23 @@ package com.keelim.arducon.activities;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
 import com.keelim.arducon.R;
+import com.keelim.arducon.databinding.ActivitySettingBinding;
 import com.keelim.arducon.fragments.SettingFragmnet;
 
 public class SettingActivity extends AppCompatActivity {
+    ActivitySettingBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) { //fragment 설정을 위한 activity
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_setting);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_setting);
+        binding.setActivity(this);
         getSupportFragmentManager()
                 .beginTransaction()
                 .replace(R.id.fragment_container, new SettingFragmnet())
                 .commit();
-
     }
 }
