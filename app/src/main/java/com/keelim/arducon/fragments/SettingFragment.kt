f29ab1151455b3b7aaa28bb4123bf1b77813c5a2
keelim <kimh00335@gmail.com>
2020-01-08 17:05:45 +0900

    2020 01 08

---
+++ app/src/main/java/com/keelim/arducon/fragments/SettingFragment.kt
@@ -0,0 +1,24 @@
+package com.keelim.arducon.fragments
+
+import android.os.Bundle
+import android.widget.Toast
+import androidx.preference.Preference
+import androidx.preference.PreferenceFragmentCompat
+import com.keelim.arducon.R
+
+class SettingFragment : PreferenceFragmentCompat() {
+    //setting
+    override fun onCreatePreferences(savedInstanceState: Bundle, rootKey: String) {
+        addPreferencesFromResource(R.xml.setting_preference)
+    }
+
+    override fun onPreferenceTreeClick(preference: Preference): Boolean {
+        val key = preference.key
+        when(key){
+            "developer" -> Toast.makeText(context, "개발 진행 중입니다.", Toast.LENGTH_SHORT).show()
+        }
+        return super.onPreferenceTreeClick(preference)
+    }
+
+
+}
