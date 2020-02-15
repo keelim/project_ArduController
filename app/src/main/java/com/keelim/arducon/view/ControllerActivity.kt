b6a388d614fbf13c44ad81f707ab06beb2ddc4c6
h0033 <kimh00335@gmail.com>
2020-02-16 01:14:29 +0900

    2020 02 16

--- app/src/main/java/com/keelim/arducon/view/ControllerActivity.kt
+++ app/src/main/java/com/keelim/arducon/view/ControllerActivity.kt
@@ -5,22 +5,18 @@ import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.keelim.arducon.R
+import kotlinx.android.synthetic.main.activity_controller.*
 
-class ControllerActivity : AppCompatActivity(), View.OnClickListener {
+class ControllerActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_controller)
         Toast.makeText(this, "컨트롤러 설정 입니다.", Toast.LENGTH_SHORT).show()
 
-    }
-
-    override fun onClick(v: View?) {
-        when (v!!.id) {
-            R.id.bt_center -> Toast.makeText(this, "center 버튼을 클릭하였습니다.", Toast.LENGTH_SHORT).show()
+        bt_center.setOnClickListener {
+            Toast.makeText(this, "center 버튼을 클릭하였습니다.", Toast.LENGTH_SHORT).show()
         }
     }
-
-
 }
 
 
--- app/src/main/java/com/keelim/arducon/view/SplashActivity.kt
+++ app/src/main/java/com/keelim/arducon/view/SplashActivity.kt
@@ -1,20 +1,25 @@
 package com.keelim.arducon.view
 
+import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
 import androidx.appcompat.app.AppCompatActivity
-import com.crashlytics.android.Crashlytics
 import com.google.android.material.snackbar.Snackbar
+import com.google.android.play.core.appupdate.AppUpdateManager
+import com.google.android.play.core.appupdate.AppUpdateManagerFactory
+import com.google.android.play.core.install.model.ActivityResult
+import com.google.android.play.core.install.model.AppUpdateType
+import com.google.android.play.core.install.model.UpdateAvailability
 import com.keelim.arducon.R
-import io.fabric.sdk.android.Fabric
 import kotlinx.android.synthetic.main.activity_intro.*
 
 class SplashActivity : AppCompatActivity() {
+    private lateinit var appUpdateManager: AppUpdateManager
     //인트로 액티비티를 생성한다.
     private var handler: Handler = Handler()
     //인앱 업데이트 어디서 등록을 해야 하는가?
-    var runnable = Runnable {
+    private var runnable = Runnable {
         //runable 작동을 하고 시작
         val intent = Intent(this, MainActivity::class.java)
         startActivity(intent) //인텐트를 넣어준다. intro -> main
@@ -26,6 +31,30 @@ class SplashActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_intro)
         Snackbar.make(container_splash, "아두콘에 오신 것을 환영 합니다.", Snackbar.LENGTH_SHORT).show()
+        //appUpdate
+
+        appUpdateManager = AppUpdateManagerFactory.create(this)
+        val appUpdateInfoTask = appUpdateManager.appUpdateInfo
+
+        appUpdateInfoTask.addOnSuccessListener { appUpdateInfo ->
+            if (appUpdateInfo.updateAvailability() == UpdateAvailability.UPDATE_AVAILABLE &&
+                    appUpdateInfo.isUpdateTypeAllowed(AppUpdateType.FLEXIBLE)) {
+                appUpdateManager.startUpdateFlowForResult(
+                        // Pass the intent that is returned by 'getAppUpdateInfo()'.
+                        appUpdateInfo,
+                        // Or 'AppUpdateType.FLEXIBLE' for flexible updates.
+                        AppUpdateType.FLEXIBLE,
+                        // The current activity making the update request.
+                        this,
+                        // Include a request code to later monitor this update request.
+                        2
+                )
+                Snackbar.make(container_splash, "업데이트를 시작합니다.", Snackbar.LENGTH_SHORT).show()
+            } else Snackbar.make(container_splash, "최신 버전 어플리케이션 사용해주셔서 감사합니다.", Snackbar.LENGTH_SHORT).show()
+        }
+
+
+        Thread.sleep(3000)
         handler.postDelayed(runnable, 1000) //handler를 통하여 사용
     }
 
@@ -33,4 +62,33 @@ class SplashActivity : AppCompatActivity() {
         super.onBackPressed()
         handler.removeCallbacks(runnable)
     }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == 2) {
+            when (resultCode) {
+                RESULT_OK -> {
+                    Snackbar.make(container_splash, "업데이트를 성공적으로 완료했습니다.", Snackbar.LENGTH_LONG)
+                }
+                Activity.RESULT_CANCELED -> {
+                    Snackbar.make(container_splash, "업데이트를 취소하였습니다.", Snackbar.LENGTH_LONG)
+                }
+                ActivityResult.RESULT_IN_APP_UPDATE_FAILED -> {
+                    Snackbar.make(container_splash, "시스템 오류가 발생했습니다.", Snackbar.LENGTH_LONG)
+                }
+            }
+        }
+    }
+
+
+    private fun popupSnackbarForCompleteUpdate() {
+        Snackbar.make(
+                container_splash, "업데이트를 다운로드 하고 있습니다.", Snackbar.LENGTH_INDEFINITE
+        ).apply {
+            setAction("RESTART") { appUpdateManager.completeUpdate() }
+            setActionTextColor(resources.getColor(R.color.colorAccent, this@SplashActivity.theme))
+            show()
+        }
+    }
+
 }
