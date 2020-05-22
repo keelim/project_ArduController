6a00de3a107624b560328ac107728a97b6a821d7
h0033 <kimh00335@gmail.com>
2020-05-22 13:41:12 +0900

    2020 05 22

---
+++ app/src/main/java/com/keelim/testing/ScrollingActivity.kt
@@ -0,0 +1,21 @@
+package com.keelim.testing
+
+import android.os.Bundle
+import com.google.android.material.appbar.CollapsingToolbarLayout
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_scrolling.*
+
+class ScrollingActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_scrolling)
+        setSupportActionBar(toolbar)
+        toolbar_layout.title = title
+        fab.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show()
+        }
+    }
+}
