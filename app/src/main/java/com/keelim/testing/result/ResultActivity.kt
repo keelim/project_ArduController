92acc7470d662ac2236caccc592f00d4fa3ffb91
h0033 <kimh00335@gmail.com>
2020-05-25 21:25:00 +0900

    2020 05 25

--- app/src/main/java/com/keelim/testing/result/ResultActivity.kt
+++ app/src/main/java/com/keelim/testing/result/ResultActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
 import android.view.View
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import com.keelim.testing.R
 import com.keelim.testing.utils.BackPressCloseHandler
@@ -13,6 +14,19 @@ class ResultActivity : AppCompatActivity(), View.OnClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_result)
+
+        //
+        AlertDialog.Builder(this)
+            .setMessage("결과를 전송")
+            .setNegativeButton("아니오") { _, _ ->
+
+            }
+            .setPositiveButton("예") { _, _ ->
+
+            }
+            .create()
+            .show()
+
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -27,7 +41,10 @@ class ResultActivity : AppCompatActivity(), View.OnClickListener {
         BackPressCloseHandler(this).onBackPressed()
     }
 
-    override fun onClick(v: View) {}
+    override fun onClick(v: View) {
+
+    }
+
     override fun onActivityResult(
         requestCode: Int,
         resultCode: Int,
