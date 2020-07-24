cc41c06b6885781a00c76c926a5b86bc71d10568
h0033 <kimh00335@gmail.com>
2020-07-24 19:40:20 +0900

    2020 07 24

--- app/src/main/java/com/keelim/hardware/view/MainActivity.kt
+++ app/src/main/java/com/keelim/hardware/view/MainActivity.kt
@@ -1,41 +1,51 @@
 package com.keelim.hardware.view
 
 import android.os.Bundle
-import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.appcompat.app.AppCompatActivity
+import androidx.viewpager.widget.ViewPager
+import com.google.android.material.tabs.TabLayout
 import com.keelim.hardware.R
-import com.keelim.hardware.view.base.BaseActivity
-import com.keelim.hardware.model.TextAdapter
-import com.keelim.hardware.utils.BackPressCloseHandler
+import com.keelim.hardware.model.PagerAdapter
+
+
 import kotlinx.android.synthetic.main.activity_main.*
-import java.util.*
 
-class MainActivity : BaseActivity() {
+class MainActivity : AppCompatActivity() {
 
-    private var items = arrayOf("Vibration Test", "Check Version Info", "SIM Card", "Proximity Sensor",
-            "Flash Light", "Touch Sensor", "Display", "Light Sensor", "Pressure Sensor"
-            , "Phone Buttons", "Speaker Test", "Wi-Fi Address", "Bluetooth Address", "Gravity sensor", "Magnetic Sensor", "Headphone",
-            "Gyroscope", "GPS Location", "Battery Indicator", "Accelarometer", "LAB Activity")
-
-    private lateinit var backPressCloseHandler: BackPressCloseHandler
+    private lateinit var adapter: PagerAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        backPressCloseHandler = BackPressCloseHandler(this)
+        tabLayout.apply {
+            addTab(tabLayout.newTab().setText(""))
+//            addTab(tabLayout.newTab().setText(""))
+//            addTab(tabLayout.newTab().setText(""))
 
-        recycler1.layoutManager = LinearLayoutManager(this)
-        val arrayList = ArrayList<String>()
-        for(a in items ){
-            arrayList.add(a)
+            tabGravity = TabLayout.GRAVITY_FILL
         }
 
-        val adapter = TextAdapter(this, arrayList)
-        recycler1.adapter = adapter
-    }
+        adapter = PagerAdapter(supportFragmentManager)
+        viewpager.adapter = adapter
+        viewpager.addOnPageChangeListener(object : ViewPager.OnPageChangeListener {
+            override fun onPageScrollStateChanged(state: Int) {
+                TODO("Not yet implemented")
+            }
 
+            override fun onPageScrolled(
+                position: Int,
+                positionOffset: Float,
+                positionOffsetPixels: Int
+            ) {
+                TODO("Not yet implemented")
+            }
 
-    override fun onBackPressed() {
-        backPressCloseHandler.onBackPressed()
+            override fun onPageSelected(position: Int) {
+                TODO("Not yet implemented")
+            }
+
+        })
+
     }
 }
