9e9262e1e210bfbbdd3ec59eb17a6e5c9ebc0720
keelim <kimh00335@gmail.com>
2020-07-31 11:41:31 +0900

    2020 07 31

--- app/src/main/java/com/keelim/hard/view/ui/SettingsFragment.kt
+++ app/src/main/java/com/keelim/hard/view/ui/SettingsFragment.kt
@@ -6,7 +6,6 @@ import android.os.Bundle
 import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
 import com.keelim.hard.R
-import com.keelim.hard.view.OpenSourceActivity
 
 class SettingsFragment : PreferenceFragmentCompat() {
 
@@ -15,21 +14,14 @@ class SettingsFragment : PreferenceFragmentCompat() {
     }
 
     override fun onPreferenceTreeClick(preference: Preference?): Boolean {
-        when (preference!!.key) {
+        return when (preference!!.key) {
             "github" -> {
                 Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/keelim/TestingHardWare")).apply {
                     startActivity(this)
                 }
-                return true
+                true
             }
-
-            "opensource" -> {
-                Intent(requireActivity(), OpenSourceActivity::class.java).apply {
-                    startActivity(this)
-                }
-                return true
-            }
-            else -> return false
+            else -> false
         }
     }
 
