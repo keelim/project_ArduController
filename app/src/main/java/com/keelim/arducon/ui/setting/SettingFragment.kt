4ecfdfc76fc83e1b9ac2ea2cdcd6696030ac30a0
keelim <kimh00335@gmail.com>
2020-12-13 22:56:21 +0900

    gitflow-feature-stash: 20201213

--- app/src/main/java/com/keelim/arducon/ui/setting/SettingFragment.kt
+++ app/src/main/java/com/keelim/arducon/ui/setting/SettingFragment.kt
@@ -1,28 +1,34 @@
 package com.keelim.arducon.ui.setting
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
+import com.google.android.play.core.review.ReviewInfo
+import com.google.android.play.core.review.ReviewManager
 import com.google.android.play.core.review.ReviewManagerFactory
 import com.keelim.arducon.R
 
 class SettingFragment : PreferenceFragmentCompat() {
+    private lateinit var  manager: ReviewManager
+    private lateinit var reviewInfo:ReviewInfo
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.root_preferences, rootKey)
+        manager = ReviewManagerFactory.create(requireActivity())
+        val request = manager.requestReviewFlow()
+        request.addOnCompleteListener { request ->
+            if (request.isSuccessful) {
+                reviewInfo = request.result
+            }
+        }
     }
 
     override fun onPreferenceTreeClick(preference: Preference): Boolean {
         when (preference.key) {
-            "setting" -> {
-                val manager = ReviewManagerFactory.create(requireContext())
+            "review" -> {
+                val flow = manager.launchReviewFlow(requireActivity(), reviewInfo)
+                flow.addOnCompleteListener { Toast.makeText(requireActivity(), "Thank you!!", Toast.LENGTH_SHORT).show() }
 
-                manager.requestReviewFlow().apply {
-                    addOnCompleteListener {
-                        if (this.isSuccessful) {
-                            manager.launchReviewFlow(requireActivity(), this.result)
-                        }
-                    }
-                }
             }
         }
         return super.onPreferenceTreeClick(preference)
